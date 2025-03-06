e4e535db8a5588d428bf85dd67fba082b16d4708
SA Omene <eserogheneo@gmail.com>
2025-03-06 05:20:35 -0600

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,7 +9,7 @@ function App(){
     return (
         <Router>
             <Switch>
-                 <Route exact path="/">
+                <Route exact path="/">
                     <Launch />
                 </Route>
                 <Route exact path="/home">
