7c98a5deaf1b5c97caa17c677b1db7c590caeedd
SA Omene <eserogheneo@gmail.com>
2025-04-28 15:59:08 -0500

    Create Shoes.jsx

---
+++ src/pages/Shoes.jsx
@@ -0,0 +1,16 @@
+import Logo from "../components/Logo";
+import SignUpBar from "../components/SignUpBar";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+
+function Shows(){
+    return(
+        <div>
+            <SignUpBar />
+            <Logo />
+            <Navbar />
+            <Footer />
+        </div>
+    );
+}
+export default Shows
